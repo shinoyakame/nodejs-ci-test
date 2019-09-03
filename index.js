339be1c7a3991018539897e97d05b32e057bb200
shinoyakame <shinoyakame02@gmail.com>
2019-09-03 13:43:53 +0700

    yarn local test sample install /w jest

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const express = require('express')
-const app = express()
-const port = 3000
+function sum(a, b){
+    return a+b;
+}
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+module.exports = sum;
