36fd8454dcdc6c80ec73b3ac13be4d02f9a5a2c0
shinoyakame <shinoyakame02@gmail.com>
2019-11-02 15:06:48 +0700

    Add minus test and upgrade node version

--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
-const sum = require('./index');
+const math = require('./index');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(math.sum(1, 2)).toBe(3);
+  });
+
+  test('minus 3 - 2 to equal 1', () => {
+    expect(math.minus(3, 2)).toBe(1);
   });
